b719e2845e84405034b10aed84a308d7efbe416a
Greg <greg.pounds3@gmail.com>
2023-09-19 05:38:34 -0500

    card make up

--- app/components/Center.js
+++ app/components/Center.js
@@ -5,13 +5,13 @@ import RecipeCard from "./RecipeCard";
 const Center = ({ recipes, announcement }) => {
   // console.log(announcement)
   return (
-    <div className="flex-grow min-h-screen m-4 pt-16 pb-14 overflow-y-scroll scrollbar-hide">
+    <div className="flex-grow min-h-screen m-4 pt-10 pb-14 overflow-y-scroll scrollbar-hide">
       <Hero/>
       {announcement && (
         <div className="flex justify-center"> Show announcements here!</div>
       )}
-      <div className="flex justify-center">
-        <div className="flex flex-col">
+      <div className="flex grid-cols-3 justify-center">
+        <div className="flex flex-wrap">
           {recipes?.map((recipe, i) => (
             <RecipeCard id={recipe?.fields.slug || i} recipe={recipe} />
           ))}
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,22 +1,22 @@
 const Hero = () => {
   return (
-    <section class="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1642112177610-c1ff271857dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1978&q=80')] bg-gray-700 bg-blend-multiply">
-      <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-          Coming  To Your Table Soon
+    <section className="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1642112177610-c1ff271857dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1978&q=80')] bg-gray-700 bg-blend-multiply">
+      <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+          Coming To Your Table Soon
         </h1>
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
           Here we focus on dishes where shock, awe, and innovation realistically
           meets your table, and touches your soul. Do you have Thyme?
         </p>
-        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
             href="#"
-            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Get to Cooking
             <svg
-              class="w-3.5 h-3.5 ml-2"
+              className="w-3.5 h-3.5 ml-2"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -33,7 +33,7 @@ const Hero = () => {
           </a>
           <a
             href="#"
-            class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+            className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
           >
             Learn more
           </a>
