1a8b9bcf66efd898b3be3dc0ebaa540a123b7a33
Greg <greg.pounds3@gmail.com>
2023-09-20 17:40:02 -0500

    bg image fix

--- app/components/PhotoCard.js
+++ app/components/PhotoCard.js
@@ -25,8 +25,8 @@ const PhotoCard = ({ photos }) => {
       <div>
         <div className="sm:min-w-[300px] md:min-w-[500px] lg:min-w-[800px] xl:min-w-[1000px] lg:min-h-[600px]  sm:min-h-[300px] w-full m-auto py-16 px-4 relative group">
           <div
-          style={{ backgroundImage: `url(http://localhost3000${photos[currentIndex]?.fields?.file?.url})` }}
-          className="top-0 left-0 w-full h-full rounded-2xl bg-center bg-cover duration-500">
+          style={{ backgroundImage: `url(${photos[currentIndex]?.fields?.file?.url})` }}
+          className="absolute top-0 left-0 w-full h-full rounded-2xl bg-center bg-cover duration-500">
             {/* <ContentfulImage
               className="sm:min-w-[300px] md:min-w-[500px] lg:min-w-[800px] xl:min-w-[1000px] lg:min-h-[600px]  sm:min-h-[300px] absolute"
               alt={photos?.[currentIndex]?.fields?.file?.url}
