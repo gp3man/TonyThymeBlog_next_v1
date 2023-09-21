8402325b42fbf63245181925c4dae8090cefbc40
Greg <greg.pounds3@gmail.com>
2023-09-20 19:23:58 -0500

    fix center

--- app/components/Center.js
+++ app/components/Center.js
@@ -5,15 +5,23 @@ import Announcement from "./AnnouncementCard";
 
 const Center = ({ recipes, announcement }) => {
   return (
-    <div className="flex-grow min-h-screen m-4 pt-10 pb-14 overflow-y-scroll scrollbar-hide">
+    <div className="flex-grow min-h-screen m-4 pt-10 pb-10 overflow-y-scroll scrollbar-hide">
       <Hero />
       {announcement ? <Announcement data={announcement} /> : null}
       <div className="flex justify-center">
-        <div className="flex flex-wrap">
+        {recipes ?
+        <div className="flex flex-wrap py-4 justify-center">
           {recipes?.map((recipe, i) => (
             <RecipeCard key={recipe?.fields.slug || i} recipe={recipe} />
           ))}
         </div>
+
+        :
+        <div className="text-center text-4xl py-6 font-bold content-center h-1/2">
+          Come Back Soon!<br/>
+          <span className="font-normal text-lg text-gray-400">No Recipes Posted Yet</span>
+        </div>
+        }
       </div>
     </div>
   );
