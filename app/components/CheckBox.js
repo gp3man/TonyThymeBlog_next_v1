40193f436d4b32ef4a946f839300b8fdc234b057
Greg <greg.pounds3@gmail.com>
2023-09-29 04:57:27 -0500

    fix ingridents list

--- app/components/CheckBox.js
+++ app/components/CheckBox.js
@@ -1,26 +1,33 @@
-"use client"
+"use client";
 import { FaCheckCircle } from "react-icons/fa";
 import { useEffect, useState } from "react";
-const Checkbox = ({text, id}) => {
+const Checkbox = ({ text, id }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [customChecked, setClass] = useState("");
-  useEffect(()=>{
-    if(isChecked){
-      setClass("customChecked")
+  useEffect(() => {
+    if (isChecked) {
+      setClass("customChecked");
+    } else {
+      setClass("");
     }
-    else{
-      setClass("")
-    }
-  },[isChecked, customChecked])
+  }, [isChecked, customChecked]);
   return (
-    <label htmlFor={id} className="cursor-pointer relative flex flex-row items-center bottom-0 p-3">
+    <label
+      htmlFor={id}
+      className="cursor-pointer relative flex flex-row items-center bottom-0 p-3"
+    >
       <input
         type="checkbox"
         id={id}
-        onChange={()=>{setIsChecked(!isChecked)}}
-        className= {`appearance-none h-6 w-6 border-2 rounded-full border-orange-500 checked:border-green-500 ${customChecked}` }
+        onChange={() => {
+          setIsChecked(!isChecked);
+        }}
+        className={`peer appearance-none h-6 w-6 border-2 rounded-full border-orange-500 checked:border-green-500 relative ${customChecked}`}
       />
-      <FaCheckCircle className={`text-green-500 h-4 w-4 absolute bottom-2 left-1 text-opacity-0 check-custom transition ${customChecked}`}/> <span className=" pl-3">{text}</span>
+      {isChecked && (
+        <FaCheckCircle className="text-green-500 h-4 w-4 absolute bottom-4 left-4 text-opacity-100 check-custom transition" />
+      )}
+      <span className="pl-3 peer-checked:line-through peer-checked:text-green-500">{text}</span>
     </label>
   );
 };
