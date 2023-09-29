f0a5d9009207b6d57440fb0b7068588e3d88fe04
Greg <greg.pounds3@gmail.com>
2023-09-29 04:41:13 -0500

    fix ingridents

--- app/components/CheckBox.js
+++ app/components/CheckBox.js
@@ -1,7 +1,28 @@
-const checkbox = (text) => {
-  return ( <label htmlFor="custom-box" className="">
-    <input type="checkbox" id="custom-box" className="appearance-none h-32 w-32 border-2 rounded-3xl border-orange-500"></input>
-  </label> );
-}
+"use client"
+import { FaCheckCircle } from "react-icons/fa";
+import { useEffect, useState } from "react";
+const Checkbox = ({text, id}) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [customChecked, setClass] = useState("");
+  useEffect(()=>{
+    if(isChecked){
+      setClass("customChecked")
+    }
+    else{
+      setClass("")
+    }
+  },[isChecked, customChecked])
+  return (
+    <label htmlFor={id} className="cursor-pointer relative flex flex-row items-center bottom-0 p-3">
+      <input
+        type="checkbox"
+        id={id}
+        onChange={()=>{setIsChecked(!isChecked)}}
+        className= {`appearance-none h-6 w-6 border-2 rounded-full border-orange-500 checked:border-green-500 ${customChecked}` }
+      />
+      <FaCheckCircle className={`text-green-500 h-4 w-4 absolute bottom-2 left-1 text-opacity-0 check-custom transition ${customChecked}`}/> <span className=" pl-3">{text}</span>
+    </label>
+  );
+};
 
-export default checkbox;
+export default Checkbox;
--- app/recipes/[slug]/page.js
+++ app/recipes/[slug]/page.js
@@ -5,7 +5,7 @@ import RecipeDetail from "../../components/RecipeDetail";
 import RichText from "../../components/RichText";
 import Link from "next/link";
 import PhotoCard from "../../components/PhotoCard";
-
+import Checkbox from "@/app/components/CheckBox";
 export default async function RecipePage({ params, preview = false }) {
   const currentClient = preview ? previewClient : client;
   const response = await currentClient.getEntries({
@@ -28,6 +28,7 @@ export default async function RecipePage({ params, preview = false }) {
     title,
     timeToCook,
     authorsNotes,
+    timeToPrep
   } = recipe?.fields;
   return (
     <section className="flex min-h-screen m-4 pt-16 overflow-y-scroll scrollbar-hide justify-center">
@@ -39,7 +40,9 @@ export default async function RecipePage({ params, preview = false }) {
       )}
       <RecipeDetail>
         <header>
-          <h1 className="text-lg md:text-4xl font-black text-stone-50 py-1">{title}</h1>
+          <h1 className="text-lg md:text-4xl font-black text-stone-50 py-1">
+            {title}
+          </h1>
           <div className="flex flex-col justify-center content-center">
             <p className="text-sm font-thin text-gray-400 ">
               By:
@@ -65,7 +68,9 @@ export default async function RecipePage({ params, preview = false }) {
           {/* Notes */}
         </header>
         <div className="py-2 m-2 text-left">
-          <h2 className="text-base md:text-2xl font-black text-stone-300 py-3">Author Notes</h2>
+          <h2 className="text-base md:text-2xl font-black text-stone-300 py-3">
+            Author Notes
+          </h2>
           <hr className="opacity-50 border-orange-800 py-3" />
           <p className="pb-1 text-gray-300">{authorsNotes}</p>
         </div>
@@ -73,31 +78,48 @@ export default async function RecipePage({ params, preview = false }) {
         <PhotoCard photos={banners} />
         {/* DishTimes */}
         <div className="bg-orange-900 px-6 mx-4 ">
-          <div>
-            <h2 className="font-bold ">Cook Time</h2>
-            <p className="text-gray-300">{timeToCook}</p>
+          <div className="grid col-span-2 grid-flow-col">
+            {timeToPrep && (
+            <div>
+              <h2 className="font-bold ">Prep-Time</h2>
+              <p className="text-gray-300 cursor-pointer">{timeToPrep} hrs</p>
+            </div>
+            )}
+            <div>
+              <h2 className="font-bold ">Cook-Time</h2>
+              <p className="text-gray-300 cursor-pointer">{timeToCook} mins</p>
+            </div>
           </div>
+
           <div>
             <hr className="opacity-5" />
             <h2 className="font-bold ">Serves</h2>
-            <p className="text-gray-300">{serves}</p>
+            <p className="text-gray-300 cursor-pointer">{serves}</p>
           </div>
         </div>
         {/* Method Block */}
         <div className="text-left m-4">
-          <div>
-            <ul className="flex flex-col py-3 ">
-              <span className="text-base md:text-2xl font-black text-stone-300 py-3">Ingredients</span>
-              <hr className="opacity-50 border-orange-800 py-3" />
+          <div className="flex flex-col py-3 ">
+            <span className="text-base md:text-2xl font-black text-stone-300 py-3">
+              Ingredients
+            </span>
+            <hr className="opacity-50 border-orange-800 py-3" />
+            <ul className="grid-cols-1 md:grid-cols-2 grid-flow-row grid">
               {ingredients.map((ingredient, i) => (
-                <li className="py-1 text-gray-300 accent-orange-500" key={ingredient + i}>
-                  <input type="checkbox" /> {ingredient}
+                <li
+                  className="py-1 text-gray-300 accent-orange-500"
+                  key={ingredient + i}
+                >
+                  {/* <input type="checkbox" /> */}
+                  <Checkbox text={ingredient} id={i} />
                 </li>
               ))}
             </ul>
           </div>
           <div className="py-3">
-            <h2 className="text-base md:text-2xl font-black text-stone-300 py-3">Directions</h2>
+            <h2 className="text-base md:text-2xl font-black text-stone-300 py-3">
+              Directions
+            </h2>
             <hr className="opacity-50 border-orange-800 py-3" />
             <RichText className="text-gray-300" content={procedure} />
           </div>
