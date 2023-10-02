62235c438913b4ffce329d512313a62060ea1037
Greg <greg.pounds3@gmail.com>
2023-10-01 21:15:26 -0500

    fix card

--- app/components/RecipeCard.js
+++ app/components/RecipeCard.js
@@ -28,11 +28,11 @@ const RecipeCard = ({ recipe }) => {
               {title}
             </h5>
           </div>
-          <div className="grid grid-flow-row grid-cols-4">
+          <div className="grid grid-flow-row grid-cols-2">
             <p className="font-normal text-stone-700 dark:text-stone-400">
               Serves {serves}
             </p>
-            <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 col-start-4">
+            <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 col-start-2">
               <MdTimelapse size={20} className="pr-1" /> {timeToCook}
             </div>
           </div>
