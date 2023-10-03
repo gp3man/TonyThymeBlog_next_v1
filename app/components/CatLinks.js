9c64e57fd2ea0e8c2ba339c5f041e86941259068
Greg <greg.pounds3@gmail.com>
2023-10-03 03:21:10 -0500

    savory

--- app/components/CatLinks.js
+++ app/components/CatLinks.js
@@ -1,20 +1,25 @@
+import Link from "next/link";
 import ContentfulImage from "./ContentfulImage";
 const CatCircles = ({ categories }) => {
   return (
     <div className="flex justify-evenly sm:-space-x-32 text-center my-2">
       {categories.map((cat, i) => (
-        <div key={i}>
-          <ContentfulImage
-            alt={cat?.fields?.thumbnail?.fields?.title}
-            src={cat?.fields?.thumbnail?.fields?.file?.url}
-            width={cat?.fields?.thumbnail?.fields?.file?.details?.image?.width}
-            height={
-              cat?.fields?.thumbnail?.fields?.file?.details?.image?.height
-            }
-            className="w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] rounded-full"
-          />
-          <p>{cat.fields.title}</p>
-        </div>
+        <Link href={`/recipes/category/${cat?.fields?.slug}`}>
+          <div key={i}>
+            <ContentfulImage
+              alt={cat?.fields?.thumbnail?.fields?.title}
+              src={cat?.fields?.thumbnail?.fields?.file?.url}
+              width={
+                cat?.fields?.thumbnail?.fields?.file?.details?.image?.width
+              }
+              height={
+                cat?.fields?.thumbnail?.fields?.file?.details?.image?.height
+              }
+              className="w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] rounded-full"
+            />
+            <p>{cat.fields.title}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
