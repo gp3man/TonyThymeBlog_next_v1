06ce23804c62d5fc595147622d330459bc4864ba
Greg <greg.pounds3@gmail.com>
2023-10-10 11:03:00 -0500

    fixing cat links

--- app/recipes/categories/page.js
+++ app/recipes/categories/page.js
@@ -2,11 +2,11 @@ import CatCircles from "@/app/components/CatLinks";
 import { getCategories } from "@/lib/getRecipes";
 
 const AllCategories = async () => {
-  const categories = await getCategories({ view: undefined });
+  const categories = await getCategories();
   return (
     <section className=" flex flex-col text-center w-screen h-screen mt-16 text-stone-50">
       All Categories
-      <CatCircles categories={categories} />
+      <CatCircles categories={categories?.items} />
     </section>
   );
 };
