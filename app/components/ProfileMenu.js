a8f3243d6a2c83df2bfa7752757f5841ada411c7
Greg <greg.pounds3@gmail.com>
2023-10-13 01:06:21 -0500

    prod secret fix

--- app/components/ProfileMenu.js
+++ app/components/ProfileMenu.js
@@ -5,14 +5,14 @@ const ProfileMenu = ({ session }) => {
     <>
       {user ? (
         <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-          <label tabIndex={0} className="btn m-1">
+          <label tabIndex={0} className="btn bg-base-300 border-base-300 hover:border-accent-focus m-1">
             <div className="w-6 md:w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
               <img className="rounded-full" src={user?.image} />
             </div>
           </label>
           <ul
             tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-52"
           >
             <li>
               <a>{"Signed In As: " + user?.name}</a>
@@ -24,14 +24,14 @@ const ProfileMenu = ({ session }) => {
         </div>
       ) : (
         <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-          <label tabIndex={0} className="btn m-1">
+          <label tabIndex={0} className="btn bg-base-300 border-base-300 hover:border-accent-focus  m-1">
             <div className="w-6 md:w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
               <img className="rounded-full" src="emptyProfile.jpg" />
             </div>
           </label>
           <ul
             tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-52"
           >
             <li>
               <Link href={"/api/auth/signin"}>Sign In</Link>
