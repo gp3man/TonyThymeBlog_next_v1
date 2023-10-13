7aa355dc6efbc80101c9217f7b44298cf3df74bd
Greg <greg.pounds3@gmail.com>
2023-10-13 07:18:54 -0500

    prisma

---
+++ app/api/user/route.js
@@ -0,0 +1,41 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    //check if email already exist
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        { user: null, message: "User with this email already exist" },
+        { status: 409 }
+      );
+    }
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        { user: null, message: "User with this username already exist" },
+        { status: 409 }
+      );
+    }
+    const hashedPW = await hash(password, 10)
+    const newUser = await db.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPW
+      },
+    });
+    return NextResponse.json({user: newUser, message: "User created successfully!"}, {status: 201});
+  } catch (error) {
+    return NextResponse.json({user: null, message: error.stack}, {status: error.status});
+  }
+}
