f8d862d3be5a5e45f761126e61f5ebb2cbfea321
Greg <greg.pounds3@gmail.com>
2024-01-01 01:37:05 -0600

    error handeling

--- app/categories/[slug]/page.js
+++ app/categories/[slug]/page.js
@@ -8,37 +8,46 @@ const SingleCategory = async ({ params }) => {
   const recipes = data?.category?.linkedFrom?.recipeCollection?.items;
   return (
     <div className=" flex-col text-center justify-center mt-16 p-9 min-h-screen">
-      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl pb-6">
-        {category?.title}
-      </h1>
-      <ul className="flex-col max-w-2xl divide-y divide-primary justify-center ">
-        {recipes?.map((recipe, i) => (
-          <li className="py-3 sm:py-4 shadow-lg px-2" key={i}>
-            <Link className="flex items-center space-x-4 rtl:space-x-reverse" href={`/recipes/${recipe?.slug}`}>
-              <div className="flex-shrink-0">
-                <Image
-                  className="w-20 h-20 rounded-full"
-                  src={recipe?.thumbnail?.url}
-                  width={recipe?.thumbnail?.width}
-                  height={recipe?.thumbnail?.height}
-                  alt={recipe?.title}
-                />
-              </div>
-              <div className="flex-1 min-w-0">
-                <p className="text-sm font-medium text-gray-900 truncate">
-                  {recipe?.title}
-                </p>
-                <p className="text-sm text-gray-500 truncate ">
-                  <span>Serves </span> {recipe?.serves}
-                </p>
-              </div>
-              <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                {recipe?.timeToCook} min
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {recipes ? (
+        <div>
+          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl pb-6">
+            {category?.title}
+          </h1>
+          <ul className="flex-col max-w-2xl divide-y divide-primary justify-center ">
+            {recipes?.map((recipe, i) => (
+              <li className="py-3 sm:py-4 shadow-lg px-2" key={i}>
+                <Link
+                  className="flex items-center space-x-4 rtl:space-x-reverse"
+                  href={`/recipes/${recipe?.slug}`}
+                >
+                  <div className="flex-shrink-0">
+                    <Image
+                      className="w-20 h-20 rounded-full"
+                      src={recipe?.thumbnail?.url}
+                      width={recipe?.thumbnail?.width}
+                      height={recipe?.thumbnail?.height}
+                      alt={recipe?.title}
+                    />
+                  </div>
+                  <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-900 truncate">
+                      {recipe?.title}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate ">
+                      <span>Serves </span> {recipe?.serves}
+                    </p>
+                  </div>
+                  <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                    {recipe?.timeToCook} min
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        "Nothing here"
+      )}
     </div>
   );
 };
