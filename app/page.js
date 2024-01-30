e04f95a284fbeab7336dbb7832280f12c549d4b6
Greg <greg.pounds3@gmail.com>
2024-01-30 01:53:20 -0600

    cut landing for now

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import LandingPage from "./components/LandingPage.js";
 import { getLanding } from "@/lib/getRecipes.js";
+import Recipes from "./recipes/page.js";
 
 export default async function Home() {
   const res = await getLanding();
   return (
     <main className="flex min-h-screen">
-      <LandingPage data={res} />
+      {/* <LandingPage data={res} /> */}
+      <Recipes />
     </main>
   );
 }
