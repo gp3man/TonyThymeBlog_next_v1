ea9b8d954acb0d65c18b418570fa436f9fcd0db1
Greg <greg.pounds3@gmail.com>
2024-02-24 08:36:37 -0600

    fix loading

--- app/components/CatLinks.js
+++ app/components/CatLinks.js
@@ -5,14 +5,14 @@ const CatCircles = ({ categories }) => {
     <div className="flex flex-wrap w-full justify-evenly sm:-space-x-32 text-center pb-3 my-3 shadow-md">
       {categories.map((cat, i) => (
         <Link key={i} href={`/categories/${cat?.sys?.id}`}>
-          <div className="flex flex-col min-w-[85px] ">
+          <div className="flex flex-col min-w-[85px] hoverCircle ">
             <Image
               alt={cat?.title}
               src={cat?.thumbnail?.url}
               width={cat?.thumbnail?.width}
               height={cat?.thumbnail?.height}
               sizes="(min-width: 640px) 60px, 30px"
-              className="w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] rounded-full self-center"
+              className="w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] rounded-full self-center imgCircle"
             />
             <p className="">{cat?.title}</p>
           </div>
