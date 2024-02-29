45a6b5160f3fecf21fe1992fae8d2cf043be812e
Greg <greg.pounds3@gmail.com>
2024-02-29 10:33:55 -0600

    newsletter issue

--- app/recipes/page.js
+++ app/recipes/page.js
@@ -2,17 +2,22 @@ import SearchBar from "../components/SearchBar";
 import CatCircles from "../components/CatLinks";
 import { fetchRecipes } from "./actions";
 import InfiniteScrollRecipes from "./infinite-scroll-recipes";
+import Modal from "../modal/page";
+import NewsletterModal from "../components/modals/NewsletterModal";
 import { v4 as uuidv4 } from "uuid";
 export default async function Recipes({ searchParams }) {
   const search =
     typeof searchParams?.search === "string" ? searchParams.search : undefined;
   const { recipeCollection, categoryCollection } = await fetchRecipes({
-    search
+    search,
   });
   const categories = categoryCollection?.items;
   const recipes = recipeCollection?.items;
   return (
     <div className="min-h-screen pt-14  overflow-y-scroll scrollbar-hide justify-center">
+      <Modal>
+        <NewsletterModal />
+      </Modal>
       <header className="flex flex-col p-3 m-3 justify-center">
         <p className="font-bold text-3xl text-center">Recipes</p>
         <div>
