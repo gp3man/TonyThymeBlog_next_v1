653077e04a2e03a2bc9a08ca583869236f6f57d8
Greg <greg.pounds3@gmail.com>
2024-05-07 10:43:23 -0500

    crash fix

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import Recipes from "./recipes/page.js";
 
 export default async function Home() {
   const res = await getTour();
+  const {tourCollection}= res;
   return (
     <main className="flex m-0">
-      {res ? <LandingPage data={res} /> : <Recipes />}
+      {tourCollection.items.length ? <LandingPage data={res} /> : <Recipes />}
     </main>
   );
 }
