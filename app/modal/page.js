3406c6ec6aa8d151e570c2db52c51dfa148b143e
Greg <greg.pounds3@gmail.com>
2024-06-01 08:21:26 -0500

    fix pop up

--- app/modal/page.js
+++ app/modal/page.js
@@ -5,17 +5,23 @@ import { useEffect } from "react";
 const Modal = ({ children }) => {
   useEffect(() => {
     if (document.readyState === "complete") {
-    setTimeout(() => {
-      document.getElementById("my_modal_2").showModal();
-    }, 2000);
+      setTimeout(() => {
+        document.getElementById("my_modal_2").showModal();
+      }, 2000);
     }
   }, []);
   return (
     <dialog id="my_modal_2" className="modal modal-top sm:modal-middle">
-      <div className="modal-box bg-slate-900 hover:bg-accent">{children}</div>
-      <form method="dialog" className="modal-backdrop">
-        <button>close</button>
-      </form>
+      <div className="modal-box bg-slate-900 hover:bg-accent">
+        <div>
+          {children}
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-slate-50 font-bold bg-slate-800 bg-opacity-50 hover:bg-opacity-100 hover:bg-red-800">
+            ✕
+          </button>
+        </form>
+      </div>
     </dialog>
   );
 };
